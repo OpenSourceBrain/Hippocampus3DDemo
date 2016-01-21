9427680812114195cf9833939aabcbefff47d41b
Padraig Gleeson <p.gleeson@gmail.com>
2016-01-21 12:48:06 +0000

    Initial nC project

---
+++ neuroConstruct/generatedNeuroML2/runsim.sh
@@ -0,0 +1,2 @@
+export JNML_HOME=/home/padraig/neuroConstruct/jNeuroMLJar
+/home/padraig/neuroConstruct/jNeuroMLJar/jnml /home/padraig/git/Hippocampus3DDemo/neuroConstruct/generatedNeuroML2/LEMS_Hippocampus.xml
